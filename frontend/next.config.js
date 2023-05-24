37cce26d7d3b9e14cbb4743d380fb16d1a6425b2
NeedToUpdate <updatethatpl0x@gmail.com>
2023-05-24 14:55:11 -0400

    updated config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
